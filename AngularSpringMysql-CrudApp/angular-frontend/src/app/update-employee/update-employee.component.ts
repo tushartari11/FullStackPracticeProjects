9e61ffd31e5fe59998feba3d1765889e16f0eb53
Tushar Tari <tushartari11@gmail.com>
2024-12-12 15:19:25 +0100

    adding Source Files

---
+++ AngularSpringMysql-CrudApp/angular-frontend/src/app/update-employee/update-employee.component.ts
@@ -0,0 +1,46 @@
+import { Component, OnInit } from '@angular/core';
+import { Employee } from '../employee';
+import { EmployeeService } from '../employee.service';
+import { ActivatedRoute, Router } from '@angular/router';
+
+@Component({
+  selector: 'app-update-employee',
+  standalone: false,
+
+  templateUrl: './update-employee.component.html',
+  styleUrl: './update-employee.component.css',
+})
+export class UpdateEmployeeComponent implements OnInit {
+  id!: number;
+  employee: Employee = new Employee();
+
+  constructor(
+    private employeeService: EmployeeService,
+    private route: ActivatedRoute,
+    private router: Router
+  ) {}
+
+  ngOnInit(): void {
+    this.id = this.route.snapshot.params['id'];
+    this.employeeService.getEmployeeById(this.id).subscribe(
+      (data) => {
+        this.employee = data;
+      },
+      (error) => console.log(error)
+    );
+  }
+
+  onSubmit() {
+    console.log(this.employee);
+    this.employeeService.updateEmployee(this.id, this.employee).subscribe(
+      (data) => {
+        this.goToEmployeeList();
+      },
+      (error) => console.log(error)
+    );
+  }
+
+  goToEmployeeList() {
+    this.router.navigate(['/employees']);
+  }
+}
